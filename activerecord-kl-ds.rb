13795afad74b081aa0f1382cce7b6f8b087cc87a
kmmontes <kmontes4989@gmail.com>
2022-04-22 10:51:56 -0700

    Initial commit

---
+++ activerecord-kl-ds.rb
@@ -0,0 +1,24 @@
+
+# Set Up
+
+# Create a new Rails app named 'rolodex_challenge'.
+# Create the database. The output in the terminal should look like this:
+# Created database 'rolodex_development'
+# Created database 'rolodex_test'
+# Generate a model called Person with a first_name, last_name, and phone. All fields should be strings.
+
+# Run a migration to set up the database.
+# Open up Rails console.
+
+# Actions
+
+# Add five family members into the Person table in the Rails console.
+# Retrieve all the items in the database.
+# Add yourself to the Person table.
+# Retrieve all the entries that have the same last_name as you.
+# Update the phone number of the last entry in the database.
+# Retrieve the first_name of the third Person in the database.
+# Stretch Challenges
+
+# Update all the family members with the same last_name as you, to have the same phone number as you.
+# Remove all family members that do not have your last_name.
